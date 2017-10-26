546ab7227756f868a350efeb4a84ab5335a49ead
Erick <rick.mutua12@gmail.com>
2017-10-26 13:36:24 +0300

    update minor modifications

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
-
+from datetime import
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -67,6 +67,8 @@ class User(UserMixin, db.Model):
 
     password_hash = db.Column(db.String(255))
 
+    reviews = db.relationship('Review', backref='user', lazy="dynamic")
+
     @property
     def password(self):
         raise AttributeError('You cannot read the password attribute')
@@ -91,3 +93,26 @@ class Role(db.Model):
 
     def __repr__(self):
         return f'User { self.name }'
+
+
+class Review(db.Model):
+
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key = True)
+    movie_id = db.Column(db.Integer)
+    movie_title = db.Column(db.String)
+    image_path = db.Column(db.String)
+    movie_review = db.Column(db.Strinf)
+
+    posted = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def save_reviews(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_reviews(cls, id):
+        reviews = Review.query.filter_by(movie_id=id).all()
+        return reviews
